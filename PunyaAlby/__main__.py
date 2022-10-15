0e8b37abc02f40467e5114cebc28c71dc8d17815
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2022-10-15 22:33:14 +0700

    Update __main__.py

--- PunyaAlby/__main__.py
+++ PunyaAlby/__main__.py
@@ -6,6 +6,7 @@
 # <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
 #
 # t.me/SharingUserbot & t.me/Lunatic0de
+# t.me/ruangdiskusikami & t.me/ruangprojects 
 
 import importlib
 
@@ -13,8 +14,9 @@ from pyrogram import idle
 from uvloop import install
 
 from config import BOT_VER, CMD_HANDLER
-from PunyaAlby import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
-from PunyaAlby.helpers.misc import create_botlog, git, heroku
+from PunyaAlby import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bots
+from PunyaAlby.helpers.misc import heroku
+from PunyaAlby.modules import ALL_MODULES
 
 MSG_ON = """
 **ALBY-PYROBOT DIAKTIFKAN**📍
@@ -34,30 +36,13 @@ async def main():
             bot.me = await bot.get_me()
             await bot.join_chat("ruangdiskusikami")
             await bot.join_chat("ruangprojects")
-            try:
-                await bot.send_message(
-                    BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER)
-                )
-            except BaseException:
-                pass
+            await bot.send_message(BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER))
             LOGGER("PunyaAlby").info(
                 f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
             )
         except Exception as a:
             LOGGER("main").warning(a)
     LOGGER("PunyaAlby").info(f"ALBY-PYROBOT v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
-    if bot1 and not str(BOTLOG_CHATID).startswith("-100"):
-        await create_botlog(bot1)
-    if bot1 and str(BOTLOG_CHATID).startswith("-100"):
-        bot1.me = await bot1.get_me()
-        chat = await bot1.get_chat(BOTLOG_CHATID)
-        desc = "GROUP LOGS UNTUK ALBY-PYROBOT.\n\nHARAP JANGAN KELUAR DARI GROUP INI.\n\n✨ Powered By ~ @ruangdiskusikami ✨"
-        lolo = f"LOGS FOR {bot1.me.first_name}"
-        if chat.description != desc:
-            await bot1.set_chat_description(BOTLOG_CHATID, desc)
-        if chat.title != lolo:
-            await bot1.set_chat_title(BOTLOG_CHATID, lolo)
-        await bot1.set_chat_photo(BOTLOG_CHATID, photo="PunyaAlby/helpers/homepage.png")
     await idle()
     await aiosession.close()
 
@@ -65,6 +50,5 @@ async def main():
 if __name__ == "__main__":
     LOGGER("PunyaAlby").info("Starting ALBY-PYROBOT")
     install()
-    git()
     heroku()
     LOOP.run_until_complete(main())

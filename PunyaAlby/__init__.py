b4f2ca742e6e785bddb51d51db170315489f9bbe
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2022-11-20 18:10:47 +0700

    Update __init__.py

--- PunyaAlby/__init__.py
+++ PunyaAlby/__init__.py
@@ -106,7 +106,7 @@ bot1 = (
         api_id=API_ID,
         api_hash=API_HASH,
         session_string=STRING_SESSION1,
-        plugins=dict(root="WhyzuProject/modules"),
+        plugins=dict(root="PunyaAlby/modules"),
     )
     if STRING_SESSION1
     else None
